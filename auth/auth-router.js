6a76461dd097aa9564e5fa29f7c64477e1ce3571
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-25 15:41:38 -0500

    [Add] Added password salting & hash to auth/register endpoint, tested working.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const Users = require('../users/user-models');
-
+const bcrypt =  require('bcryptjs');
 const router = express.Router();
 
 
 router.post('/register', (req, res) => {
     let user = req.body;
+
+    const salt = bcrypt.genSaltSync(10); 
+    const hash = bcrypt.hashSync(user.password, salt);
+    
+    user.password = hash;
 
     Users.add(user)
     .then(user => {
