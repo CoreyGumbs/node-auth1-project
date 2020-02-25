a33a88138fb2fe5b47fdf5dff1430a73d4c5a0e3
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-02-25 17:06:51 -0500

    [Add] Added middlewear and code clearn up

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const router = require('./router/router');
 
 const server = express();
 
+server.use(helmet());
+server.use(cors());
 server.use(express.json());
+
 
 server.use('/api', router);
 
